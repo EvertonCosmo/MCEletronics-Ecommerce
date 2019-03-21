3811718b71e1926f3ac1a31743ba870f4136913a
Everton Cosmo <everton8cosmo@gmail.com>
2019-03-21 00:58:32 -0300

    component Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class="nav-bar">
+        <span>MC Eletronics</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NavBar',
+     props:{ msg:String}
+    }
+
+</script>
+
+<style>
+
+    .nav-bar{
+        background: linear-gradient(-90deg,#033076);
+        box-shadow: 1px 2px 2px 2px #033076;
+        height: 80px;
+        width: 100%;
+        margin-bottom: 0px;
+    }
+    span{
+        color:white;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 2.3rem;
+        margin-left: 10px;
+        
+    }
+</style>
+
+
