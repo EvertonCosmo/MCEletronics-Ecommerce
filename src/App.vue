825a1753d27d7f9e093bd87d82068a7cc8378054
Everton Cosmo <everton8cosmo@gmail.com>
2019-03-25 15:02:05 -0300

    Registration, Login and Erro functionality are completed
    * add validation functions and fixed responsive mobile phones
    
    * router link fixed
    
    * routers add
    
    * removed unused dependencies
    
    * removed scripts vue submit not used
    
    * clear the App
    
    * new routes add
    
    * improvements in register method, localstorage used for save user
    
    * function sleep() add, fixed bugs in login action, now spinner works
    
    * call all login function, better communication
    
    * data between PageLogin boosted with logout add
    
    * router used and title like props
    
    * new changes to cached dependencies, recommended to remove

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,35 @@
 <template>
-    <div id="app">
-      <!-- <div id="nav"> -->
-        <!-- <Login></Login> -->
-         
-        <!-- if true go to home page(Login) -->
 
-          <router-link v-if="authenticated" to="/Login" class="float-right" v-on:click.native = "logout()" replace>
-                Logout
-          </router-link>
-          <!-- directive v-on listening event of authenticated -->
-           <router-view @authenticated="setAuthenticated" /> 
+    <div id="app">
+   
+          <LogonBridge></LogonBridge>
+
           
-          
-      <!-- </div> -->
-       <!-- <router-link to="/login">
-           Login
-           </router-link> -->
+      
     </div>
 </template>
 
 <script>
 
-// import Login from './components/Login.vue'
-
+import LogonBridge from './components/LogonBridge'
 export default {
   name: 'app',
-  // components:{
-  //   Login
-  // },
+
+  components:{
+    LogonBridge
+
+  },
   data(){
     return {
-       authenticated: false, 
-       mockAccount:{ // fake account to simulate login 
-          email:"everton@gmail.com",
-          password:'cosmo123'
-       }
-    }
-  },
-  mounted(){ // initialize component 
-    if(!this.authenticated){ // listen the emit event of Login.vue
-      this.$router.replace({name:'Login'}); // // replace router(page) to Login likes initial page
+     
       
     }
-  },
-  methods:{
-    setAuthenticated(status){
-      this.authenticated = status;
-    },
-    logout(){
-      this.authenticated = false;
-    }
   }
-
-
+  
+  
 }
 </script>
 
 <style >
-.float-right{
-  color:white;
-  text-decoration: none;
-  font-size: 2em;
-  margin-right: 20px;
-  
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+/* eslint-disable */ 
+// eslint-disable-next-line 
+
 import Vue from 'vue'
 import App from './App.vue'
 
 import BootstrapVue from 'bootstrap-vue'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,13 +13,21 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import VueRouter from 'vue-router'
 
+// Components
 import LoginComponent from './components/Login.vue'
-import PageLoginComponent from "./components/PageLogin.vue";
+import PageLoginComponent from './components/PageLogin.vue';
+import RegisterComponent from './components/Register.vue'
+import LogonBridgeComponent from './components/LogonBridge.vue'
+
+
+
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 Vue.config.devtools = true
+
 
 
 const routes =  [
@@ -25,23 +37,37 @@ const routes =  [
           name:"Login"
       }
   },
+
   {
       path:"/Login",
       name:"Login",
       component:LoginComponent
   },
+
   {
       path:"/PageLogin",
       name:"PageLogin",
       component:PageLoginComponent
 
+  },
+  {
+    path:"/Register",
+    name:"Register",
+    component:RegisterComponent
+    
+  },
+  {
+    path:"/Bridge",
+    name:"Bridge",
+    component:LogonBridgeComponent
   }
+  
 
 ]
-const router = new VueRouter({
-  routes
 
-});
+const router = new VueRouter({routes});
+
+
 new Vue({
   el:'#app',
   router,
