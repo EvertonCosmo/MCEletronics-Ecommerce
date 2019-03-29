9d0ee8c7f981543ce2a12d30aa3f32b5d8a87702
Everton Cosmo <everton8cosmo@gmail.com>
2019-03-28 22:56:52 -0300

    bar search fixed double background-color

--- src/components/MainBar.vue
+++ src/components/MainBar.vue
@@ -2,25 +2,25 @@
     <div>
       <!-- <b-navbar style="position: relative max-width:25rem" class="nav-bar" toggleable="lg" type="dark" variant="info" > -->
         <!-- <NavBar></NavBar> -->
-     <header>
+     <header>   
       <b-navbar toggleable="lg" class="box-search">
 
         <b-navbar-toggle target="nav_collapse" />
         <b-navbar-brand style="font-size: 120%; position: relative; color: white" class="logo" href="PageHome">MC Eletronics</b-navbar-brand>
-        <b-collapse is-nav style="width: 100% border:none" id="nav_collapse">
+        <b-collapse  e is-nav style="width: 100%" id="nav_collapse">
 
           <div id="navigation">
-         
-             
+            <b-nav-form>
+              <b-form-group id="form-search" class="required-field">
                 <b-input-group >
-                  <b-form-input type="text"  maxlength="10" name="search"  placeholder="Buscar... "/>
-                    <b-input-group-btn >
+                  <b-form-input type="text" style="border-radius: 15px" maxlength="10" name="search"  placeholder="Buscar... "/>
+                  <b-input-group-btn >
                     <i class="fas fa-search" style="position:absolute;right:8px;top: 9px;visibility:visible; color: #033076; font-size: 22px" ></i>
-                    </b-input-group-btn> 
+                  </b-input-group-btn> 
                 </b-input-group>           
-             
+              </b-form-group>
+            </b-nav-form> 
           </div>
-
           <div style="max-width: 25rem; width: 100%; display: block">
             <b-button  id="button-favoritos" type="submit" > 
               <i class="far fa-heart"></i>
@@ -42,11 +42,11 @@
 <script>
 
 
-import NavBar from './NavBar'
+
 export default {
     name: 'PageHome',
    components:{
-     NavBar
+    
    },
     data(){
       return {
@@ -60,84 +60,137 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 /* Style the header */
- header {
-  background: linear-gradient(-90deg,#033076);
+header {
+  background-color: #033076;
   text-align: center;
   font-size: 35px;
   color: white;
-  box-shadow: 1px 2px 2px 2px #033076;
-  height: 80px;
+  height: 2%;
+}
 
-} 
+article {
+  float: right;
+  padding: 20px;
+  width: 70%;
+  background-color: #fff;
+  height: 100px; /* only for demonstration, should be removed */
+}
 
-.nav-bar{
-        background: linear-gradient(-90deg,#033076);
-        position: fixed;
-        box-shadow: 1px 2px 2px 2px #033076;
-        height: 80px;
-        width: 100%;
-        margin-bottom: 0px;    
-        padding: 30px;
-        text-align: center;
-  
-    }
+/* Clear floats after the columns */
+section:after {
+  content: "";
+  display: table;
+  clear: both;
+  height: 100px;
+}
 
-.logo{
-        color:white;
-        position: relative;
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  
+/* Style the footer */
+footer {
+  background-color: #033076;
+  padding: 20px;
+  text-align: center;
+  color: white;
+  height: 2%;
+}
+
+/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
+@media (max-width: 600px) {
+  nav, article {
+    width: 100%;
+    height: auto;
+  }
+}
+
+#mid {
+  color: white;
+}
+
+.mais-vendidos{
+  margin: 1%;
+  background-color: rgb(222, 224, 224);
+  height: 400px;
+}
+
+.informacoes {
+  background-color: rgb(155, 154, 154);
+  margin: 0%;
+  height: 250px;
+}
+/* Create two columns/boxes that floats next to each other */
+#categorias {
+  float: left;
+  width: 30%;
+  background: white;
+  padding: 1.7%;
+  font-size: 100%;
 }
 
 #form-search{
-    position: relative;
-    width:700px;;
-    height: 40px;
-    border-radius: 10px;
-    background: #e6f3ff;
-    font-size: 18px;
+  width: 100%;
+  margin-top: 1.7%;
 }
-#button-search{
-    background-color: #e6f3ff;
-    color: #033076;
-    height: 40px;
-    font-size: 20px;
-    width: 50px;
-    border: 5px;
-    border-radius: 5px;
-}   
+/* #button-search{
+  background-color: #e6f3ff;
+  color: #033076;
+  height: 60%;
+  font-size: 45%;
+  width: 20%;
+  border: 10%;
+  border-radius: 15%;
+  height: 36px;
+}    */
 #button-favoritos{
-    background-color: #033076;
-    width: 50px;
-    height: 40px;
-    /* position: relative; */
-    font-size: 25px;
-    border: 0px;
-    margin-left: 30px;
-    border-radius: 5px;
+  background-color: #033076;
+  width: 20%;
+  height: 70%;
+  font-size: 55%;
+  border: 0px;
+  margin-left: -10%;
+  border-radius: 5%;
 }
 #button-bag{
-    background-color: #033076;
-    width: 50px;
-    height: 40px;
-    /* position: relative; */
-    font-size: 25px;
-    border: 0px;
-    margin-left: 25px;
+  background-color: #033076;
+  width: 20%;
+  height: 70%;
+  font-size: 55%;
+  border: 0px;
+  margin-left: 3%;
 }
 #purchase-value{
   color: white;
-  font-size: 20px;
-  margin-left: 25px;
-  margin-top: 10px;
+  font-size: 55%;
+  margin-left: 5%;
 }
+#navigation {
+  width: 100%;
+  margin-top: -0.5%;
+}
+
 .box-search{
-    position: fixed;
-    margin-left: 50px;
-    width: 100%;
+  position: sticky;
+  width: 100%;
 }
 
-
+.logo{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  
+}
+ .item{
+      width:100%;
+      /* padding:5%; */
+      border-left:none;
+      border-right: none;
+    }
+.item > :hover{
+        /* Background-color: #033076; */
+        background-color: #033076;
+        color:white;
+}
 </style>
 
