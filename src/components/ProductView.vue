e224a07c7e4dd4113fe1a9008e368c2609954e12
Everton Cosmo <everton8cosmo@gmail.com>
2019-03-28 23:01:14 -0300

    #3 showing  data information from product

--- src/components/ProductView.vue
+++ src/components/ProductView.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
         <main-bar></main-bar>
+
         <b-card tag="article" class="gallery-wrap " style=" margin-top:20px; border:none;max-width:60rem;height:40rem;" >
             <b-row >     
                     <aside class="col-sm-5 border-right" >
@@ -33,7 +34,7 @@
                                 <span class="price h3 text-warning"> 
                                     
                                     <span class="currency"> R$ </span>
-                                    <span class="num"> 200,00 </span>
+                                    <span class="num"> 300,00 </span>
                                 
                                 </span>
                         </p>       
@@ -44,6 +45,8 @@
                         <br>
                         <br>
                         
+                        <br>
+                        <br>
                         <hr style="max-width:50rem"> 
 
                         <!-- Credit Card -->
@@ -70,28 +73,30 @@
 
                         
                     </b-card-body>
-                
+                <label style="margin-left:25%; margin-bottom:0%">Calcular frete</label>
+                <p style="color:red">{{value}}</p>
+                 <b-form-group id="cep" style="margin-left:50%;margin-top:-5%;">
+                          <b-form-input class="input" type="text" maxlength="9" style="max-width:100%" name="input" placeholder="CEP"/>
+                          <b-button variant="primary"  style="position:absolute; left:85%; top:84.1%" class="float-right">OK</b-button>
+                </b-form-group>  
                 </b-card>
                     
                     
-                    <b-card class="col-sm-5" style="margin-left:60%;margin-top:-8.5%; max-width:40rem; height:0rem; border:none;"> 
-                            <b-card-body style="margin-left:10%; margin-top:-5%">
-                                
-                                  <b-form-group id="cep" label='Calcular Frete' label-for="input" style="margin-left:50%;margin-top:-8.5%;">
-                                        <b-form-input class="input" type="text" maxlength="8" style="max-width:100%" name="input" placeholder="CEP"/>
-                                 </b-form-group>  
-                            </b-card-body>
-                    </b-card>
+                
+                                 
+                            
                    
                     <br><br>
                    <h3 style="color:#033076">Informações do Produto</h3>
-                   <dl class="item-property text-justify " >
+                   <dl class="text-justify " >
                       <dd> <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, aspernatur fugit. Dolores, repellendus asperiores. Dolore, minima atque architecto unde voluptate voluptatem similique eveniet aut est fugiat. Dolores dolorem veniam ullam.
                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est atque, explicabo ipsa aperiam facere voluptates quod, nostrum id eum perferendis asperiores error tenetur laboriosam culpa ut accusantium et voluptas esse.
                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt beatae molestias adipisci sed et iste praesentium reprehenderit! In, corporis harum, asperiores fuga, velit quam iusto dicta similique hic necessitatibus iure.
                        
                        </p></dd>
                    </dl>
+
+                   <!-- Área de avaliação -->
                 </b-row>
                 
             </b-card>
@@ -120,15 +125,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 /* #item-display{
     width: 20%;
     height: 20%;
 } */
 
+
 .gallery-wrap .img-small-wrap .item-gallery {
     width: 15%;
-    height: 6%;
+    height:1%;
     border: 1px solid #ddd;
     margin: 5px 1px 20px 10px;
     display: inline-block;
