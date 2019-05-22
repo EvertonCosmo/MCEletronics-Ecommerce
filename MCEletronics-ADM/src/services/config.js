ad1c01b8d25a0374e7b1cd778a461527498940d6
Emanoel Alves <emanoel-alves09@hotmail.com>
2019-05-22 00:33:05 -0300

    Area do adm(front-end)

---
+++ MCEletronics-ADM/src/services/config.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+
+export const http = axios.create({
+    baseURL: 'http://localhost:8080/project/api/',
+     headers: {
+     		Accept: 'application/json',
+    	   'Content-Type': 'application/json'
+  },
+})
